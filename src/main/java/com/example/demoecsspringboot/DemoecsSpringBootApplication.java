a598c0b2b301682209392381465e9a79f273ae22
santhakumars12 <santhakumars12@outlook.com>
2023-10-23 11:14:31 +0530

    first commit

---
+++ src/main/java/com/example/demoecsspringboot/DemoecsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoecsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoecsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoecsSpringBootApplication.class, args);
+	}
+
+}
